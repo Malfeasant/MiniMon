d9e89204bf69cbafe25da36584ac4f422712656c
Mischa Sumrall <madchao@yahoo.com>
2013-07-23 07:47:53 -0700

    prints the current stack pointer
    probably won't keep this, but the bin->hex function might come in handy

--- boot/mbr.asm
+++ boot/mbr.asm
@@ -4,6 +4,25 @@ org 0x7c00
 start:
 	mov si, sAlive	; greeting
 	call stringOut	; print it
+	
+	; out of curiosity, where is the stack?
+	mov dx, sp
+	mov si, sHex + 4	; start at (actually past) the string's end and work back
+.loop:
+	dec si
+	mov al, dl
+	and al, 0xf
+	add al, '0'
+	cmp al, '9'
+	jle .skip
+	add al, 7	; correct for higher digits
+.skip:
+	mov [si], al	; put it in the buffer
+	ror dx, 4	; shift bits
+	cmp si, sHex	; back to the start yet?
+	jne .loop
+	call stringOut
+	
 	mov ax, 0x2401	; bios a20 enable
 	int 0x15	; leaves carry set if problem
 	jc a20fail	; if we can't enable a20, long mode will have a very bad time...
@@ -36,6 +55,7 @@ stringOut:	; prints a null-terminated string pointed to by SI
 sAlive	db	`Ahoy!\r\n\0`
 sHalt	db	`Halting.\r\n\0`
 sA20	db	`Failed to enable A20.\r\n`
+sHex	db	`xxxx\r\n\0`	; buffer for a cheap bin->hex conversion
 
 times 510 - ($ - $$) db 0x2b
 bootSig dw 0xaa55
